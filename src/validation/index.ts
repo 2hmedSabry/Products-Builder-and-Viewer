710cf5bd732621e8ab10cfd2950865c17be80354
2hmedsabry <ahmed.s.elsshourbagy@gmail.com>
2025-04-28 04:27:47 +0300

    Add JSDoc for productValidation function and fix description length validation error and other of error

--- src/validation/index.ts
+++ src/validation/index.ts
@@ -1,11 +1,28 @@
-// productObj === errorsOBJ(TITLE ,DESCRIPTION ,PRICE ,CATEGORY ,IMAGEURL ,RATING ,COUNTINSTOCK)
+/**
+ * Validates a product object containing title, description, image URL, and price.
+ * Returns an object with validation error messages for each field.
+ *
+ * @param {Object} product - The product object to validate.
+ * @param {string} product.title - The title of the product.
+ * @param {string} product.description - The description of the product.
+ * @param {string} product.imageURL - The image URL of the product.
+ * @param {string} product.price - The price of the product.
+ * 
+ * @returns {Object} errors - An object containing error messages for each invalid field.
+ * @returns {string} errors.title - Error message for invalid product title.
+ * @returns {string} errors.description - Error message for invalid product description.
+ * @returns {string} errors.imageURL - Error message for invalid product image URL.
+ * @returns {string} errors.price - Error message for invalid product price.
+ */
 
+
+// productObj === errorsOBJ(TITLE ,DESCRIPTION ,PRICE ,CATEGORY ,IMAGEURL ,RATING ,COUNTINSTOCK)
 
 export const productValidation = (product : {title : string , description : string , imageURL : string , price : string}) =>{
     // ** Returns an object 
     const errors : { title : string , description : string, 
         imageURL : string ,price : string } 
-     ={
+     = {
         title : "NOT VALID PRODUCT TITLE",
         description : "NOT VALID PRODUCT DESCRIPTION",
         imageURL : "NOT VALID PRODUCT IMAGE URL",
@@ -17,7 +34,7 @@ const validUrl = /^(ftp|http|https):\/\/[^ "]+$/.test(product.imageURL);
 if(!product.title.trim() || product.title.length < 10 || product.title.length > 80){
     errors.title = "Product title must be between 10 and 80 characters"; 
 }
-if(product.description.trim() || product.description.length < 10 || product.description.length > 80){
+if(!product.description.trim() || product.description.length < 10 || product.description.length > 900){
     errors.description = "Product description must be between 10 and 900 characters"; 
 }
 if(!product.imageURL.trim() || !validUrl){
